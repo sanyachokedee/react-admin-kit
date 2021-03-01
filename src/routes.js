e2122e98adfbcdc7a921341e956ed3ce4fc1f6ec
MooMOO <ing118199@gmail.com>
2021-03-01 13:55:13 +0700

    first commit

---
+++ src/routes.js
@@ -0,0 +1,32 @@
+import { Route, Switch } from 'react-router-dom'
+import Dashboard from './pages/backend/dashboard/Dashboard'
+import Product from './pages/backend/product/Product'
+import Forgotpassword from './pages/forgotpassword/Forgotpassword'
+import Login from './pages/login/Login'
+import PageNotFound from './pages/pagenotfound/PageNotFound'
+import Register from './pages/register/Register'
+
+const Routes = () => {
+
+    return (
+
+        <Switch>
+
+            {/* Frontend */}
+            <Route path="/" exact={true} component={Login} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+            <Route path="/forgotpassword" component={Forgotpassword} />
+
+            {/* Backend */}
+            <Route path="/backend/dashboard" component={Dashboard} />
+            <Route path="/backend/product" component={Product} />
+
+            <Route component={PageNotFound} />
+
+        </Switch>
+
+    )
+}
+
+export default Routes
